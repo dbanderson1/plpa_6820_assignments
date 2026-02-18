86c4506829d8af9c26c23c8081fe906dd5ee6864
dbanderson1 <derek.anderson@uwalumni.com>
2026-02-17 23:11:02 -0600

    initial save

---
+++ assignment_004_rmarkdown.R
@@ -0,0 +1,11 @@
+# install required packages for rmarkdown
+install.packages('knitr')
+install.packages('tinytex')
+install.packages('rmarkdown')
+install.packages('pandoc')
+
+# turn on packages
+library(knitr)
+library(tinytex)
+library(rmarkdown)
+library(pandoc)
